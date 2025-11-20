7ee70c25f7640e3f0c57ccb9e22750ee82e6a6cd
RLMWLKR <chasep1010@comcast.net>
2025-11-20 11:51:10 -0800

    Puzzle wall workings

---
+++ TopDownShooter/Assets/Scripts/WallDoorScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WallDoorScript : MonoBehaviour
+{
+
+    public DoorScript doorScript;
+    private float doorSpeed = 5;
+
+
+    private void Start()
+    {
+
+
+    }
+
+
+    private void Update()
+    {
+        /*
+        //If the first door needs to move on the x or y axis then move.
+        if (doorScript.MoveX1 == true)
+        {
+            if (transform.position.x)
+        }
+        else if (doorScript.MoveY1 == true)
+        {
+
+        }
+        //If the door doesn't need to move then don't
+        else
+        {
+
+        }
+        //If the second door needs to move on the x or y axis then move.
+        if (doorScript.MoveX2 == true)
+        {
+
+        }
+        else if (doorScript.MoveY2 == true)
+        {
+
+        }
+        else
+        {
+
+        }
+
+
+
+*/
+    }
+
+}
