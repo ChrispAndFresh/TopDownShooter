ec7b95ea6afac3af390d77c2a46068fbf1420ced
RLMWLKR <chasep1010@comcast.net>
2025-12-09 12:04:07 -0800

    Puzzle room updates

--- TopDownShooter/Assets/Scripts/WallDoorScript.cs
+++ TopDownShooter/Assets/Scripts/WallDoorScript.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
-
+/*
+12/9/25
+Chase Phillips
+Handles the movement and interaction of the secret wall doors.
+*/
 public class WallDoorScript : MonoBehaviour
 {
 
@@ -15,10 +21,23 @@ public class WallDoorScript : MonoBehaviour
 
     private void Update()
     {
+        StartCoroutine(WaitToErase());
+    }
 
+
+
+    /// <summary>
+    /// Moves the door if the lever is activated and then 
+    /// deactivates the door after it has moved out of the players way.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator WaitToErase()
+    {
         if (leverScript.Active)
         {
             transform.position = Vector3.MoveTowards(transform.position, moveP.position, doorSpeed * Time.deltaTime);
+            yield return new WaitForSeconds(2);
+            gameObject.SetActive(false);
         }
     }
 }
